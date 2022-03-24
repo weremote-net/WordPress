e641fa0cf2626b81c43c9baf1ba453d52778446e
Sebastián Castaño <contacto@weremote.net>
2022-03-24 15:23:54 -0500

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ *
+ * @link       http://www.example.com
+ * @since      0.0.1
+ *
+ * @package    Plugin_Name
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
